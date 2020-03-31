0c6d87dedce50c91bf29d43a3f882eedd3038126
Trevor Joseph <trevor.joseph@johnlewis.co.uk>
2020-03-31 17:06:47 +0100

    updated test

--- test/app.test.js
+++ test/app.test.js
@@ -56,26 +56,25 @@ describe('app', function() {
         expect(error.message).toEqual('You need to pass TimesTableArray() a number')
       }
     });
-  })
 
-  it('should create a grid', () => {
-    const state = [
-      { number: 1, selected: false},
-      { number: 2, selected: false},
-      { number: 3, selected: true},
-      { number: 4, selected: false},
-      { number: 5, selected: false},
-      { number: 6, selected: true},
-      { number: 7, selected: false},
-      { number: 8, selected: false},
-      { number: 9, selected: true},
-      { number: 10, selected: false},
-    ];
-    
-    document.body.innerHTML = `<div id="div1"></div>`;
-    createGrid(state)
-    const expected = `<div id="div1"><ul class="table"><li data-id="1" class="unselected"><div>1</div></li><li data-id="2" class="unselected"><div>2</div></li><li data-id="3" class="selected"><div>3</div></li><li data-id="4" class="unselected"><div>4</div></li><li data-id="5" class="unselected"><div>5</div></li><li data-id="6" class="selected"><div>6</div></li><li data-id="7" class="unselected"><div>7</div></li><li data-id="8" class="unselected"><div>8</div></li><li data-id="9" class="selected"><div>9</div></li><li data-id="10" class="unselected"><div>10</div></li></ul></div>`;
-    expect(document.body.innerHTML).toEqual(expected);
+    it('should create a grid of dom elements with given state', () => {
+      const state = [
+        { number: 1, selected: false},
+        { number: 2, selected: false},
+        { number: 3, selected: true},
+        { number: 4, selected: false},
+        { number: 5, selected: false},
+        { number: 6, selected: true},
+        { number: 7, selected: false},
+        { number: 8, selected: false},
+        { number: 9, selected: true},
+        { number: 10, selected: false},
+      ];
+      
+      document.body.innerHTML = `<div id="div1"></div>`;
+      createGrid(state)
+      const expected = `<div id="div1"><ul class="table"><li data-id="1" class="unselected"><div>1</div></li><li data-id="2" class="unselected"><div>2</div></li><li data-id="3" class="selected"><div>3</div></li><li data-id="4" class="unselected"><div>4</div></li><li data-id="5" class="unselected"><div>5</div></li><li data-id="6" class="selected"><div>6</div></li><li data-id="7" class="unselected"><div>7</div></li><li data-id="8" class="unselected"><div>8</div></li><li data-id="9" class="selected"><div>9</div></li><li data-id="10" class="unselected"><div>10</div></li></ul></div>`;
+      expect(document.body.innerHTML).toEqual(expected);
+    });
   });
-
 });
